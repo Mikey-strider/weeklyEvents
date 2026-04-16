34dba2b2c70d8953c58e98dde02651699e17465d
Dromen2001 <dromen2001@gmail.com>
2026-04-16 14:17:56 -0700

    building carousel display parameters

--- index.js
+++ index.js
@@ -3,38 +3,72 @@ function modifyContent() {
 
     weeklyEvents.innerHTML = `
       <h2>Weekly events:</h2>
+
+      <h3>Current Events</h3>
+          <div class="event-carousel">
+            <h2>CSS carousel single item per page</h2>
+            <ul class="current-placement">
+              <li data-accName="Item 1">
+                <h2>Page 1</h2>
+              </li>
+              <li data-accName="Item 2">
+                <h2>Page 2</h2>
+              </li>
+              <li data-accName="Item 3">
+                <h2>Page 3</h2>
+              </li>
+              <li data-accName="Item 4">
+                <h2>Page 4</h2>
+              </li>
+            </ul>
+          </div>
+
       <ul class="this-week">
         <li id="monday" class="week-day">
-          <h3>Monday</h3>
+          <h3 class="day-title">Monday</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/pillows4-13-288x360.jpg">
         </li>
         <li id="wednesday" class="week-day">
-          <h3>Wednesday</h3>
+          <h3 class="day-title">Wednesday</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/DMBoysTApril-278x360.jpg">
         </li>
         <li id="thursday" class="week-day">
-          <h3>Thursday</h3>
+          <h3 class="day-title">Thursday</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/UP4-16-262x360.jpg">
         </li>
         <li id="friday" class="week-day">
-          <h3>Friday</h3>
+          <h3 class="day-title">Friday</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/tradeapril-278x360.jpg">
         </li>
         <li id="saturday-happy-hour" class="week-day">
-          <h3>Saturday Happy hour</h3>
+          <h3 class="day-title">Saturday Happy hour</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/chubapril-278x360.jpg">
         </li><li id="saturday-evening" class="week-day">
-          <h3>Saturday Evening</h3>
+          <h3 class="day-title">Saturday Evening</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/BPapril-280x360.jpg">
         </li>
         <li id="sunday" class="week-day">
-          <h3>Sunday</h3>
+          <h3 class="day-title">Sunday</h3>
           <img class="event-img" src="https://powerhousebar.com/wp-content/uploads/2026/04/cgateapril2-203x360.jpg">
         </li>
         <li id="future-events" class="week-day">
           <h3>Future Events</h3>
-          <div class="place-future-events">
-            
+          <div class="event-carousel">
+            <h2>CSS carousel single item per page</h2>
+            <ul class="future-placement">
+              <li data-accName="Item 1">
+                <h2>Page 1</h2>
+              </li>
+              <li data-accName="Item 2">
+                <h2>Page 2</h2>
+              </li>
+              <li data-accName="Item 3">
+                <h2>Page 3</h2>
+              </li>
+              <li data-accName="Item 4">
+                <h2>Page 4</h2>
+              </li>
+            </ul>
           </div>
         </li>
       </ul>
@@ -72,6 +106,15 @@ function modifyContent() {
   }
 
   h3 {
+    color: #ff4040;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    text-decoration: underline;
+  }
+
+  .day-title {
     color: #ff4040;
     display: flex;
     flex-direction: row;
